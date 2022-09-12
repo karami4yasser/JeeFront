4b163b4ac3f1016f1d3966ac0a104ca3bd63f0e9
YASSER KARAMI <83478271+karami4yasser@users.noreply.github.com>
2022-09-12 16:57:59 +0100

    okk

--- components/Nav.js
+++ components/Nav.js
@@ -18,32 +18,7 @@ function Nav({count}) {
   return (
    
     <nav className="relative">
-      <div
-        className="flex px-10 
-      sm:px-20 text-2xl whitespace-nowrap 
-      space-x-10 sm:space-x-20 overflow-x-scroll 
-      scrollbar-hide"
-      >
-        {Object.entries(requests).map(([key, { title, url }]) => (
-          <h2
-            key={key}
-            onClick={() =>
-              
-              {setPage(1); setKey(key)
-               router.push(`/?category=${key}&page=${page}`)}
-              
-              
-              
-              }
-            className="cursor-pointer 
-            transition duration-100 transform hover:scale-125
-             hover:text-white  
-             last:pr-24 active:text-red-500"
-          >
-            {title}
-          </h2>
-        ))}
-      </div>
+
       <div
         className="absolute top-0 right-0 bg-gradient-to-l 
       from-[#0622A] h-10 w-1/12"
